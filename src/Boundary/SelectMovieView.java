7b8516dc3113adc2e15c80a9974d1c8b464fd4d4
martinha1909 <58370282+martinha1909@users.noreply.github.com>
2020-11-28 14:18:34 -0700

    Update LoginView.java

--- src/Boundary/SelectMovieView.java
+++ src/Boundary/SelectMovieView.java
@@ -1,11 +1,5 @@
 package Boundary;
 
-public class SelectMovieView implements View{
-
-	@Override
-	public void perform() {
-		// TODO Auto-generated method stub
-		
-	}
+public class SelectMovieView {
 
 }
